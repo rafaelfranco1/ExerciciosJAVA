082b73f44c2d231d69c152a46962f5a689c74de4
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2022-09-06 13:14:58 -0300

    Add files via upload

---
+++ DIVISAOPOR2DEZVEZES.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	
+	int = 1;
+	
+	f = 0;
+	
+	while (i <= 10) {
+		printf("Escreva um número para exibir a metade: ");
+		scanf("%f",&f);
+		printf("\n");
+		float j = f/2;
+		print("%.2f \n",j);
+		
+	}
+	
+	
+	
+	return 0;
+}
