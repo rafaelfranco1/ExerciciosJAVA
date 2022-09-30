13a98a80b3d72194a40cb6b735156251c52d4c25
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2022-09-30 08:43:26 -0300

    Usuário entrando com a populaçãoWHILE

---
+++ LoopsRepetição5.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.aula17;
+
+public class LoopsRepetição5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
