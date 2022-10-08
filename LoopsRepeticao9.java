a582819c4a8f02956a95a5d4d92db7cb33b5ee14
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2022-10-08 16:44:22 -0300

    Add files via upload

---
+++ LoopsRepeticao9.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula17;
+
+public class LoopsRepeticao9 {
+
+	
+	public static void main(String[] args) {
+		int i;
+		
+		for (i = 1 ; i < 51 ; i++) {
+			if (i % 2 == 1) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
